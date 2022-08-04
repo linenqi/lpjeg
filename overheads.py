d8f02357b98482a38bd7e3a01a9b3e47d28ff960
Enqi <linenqi797@gmail.com>
2022-08-04 20:05:28 +0800

    ggf

--- overheads.py
+++ overheads.py
@@ -6,47 +6,58 @@ response= requests.get(url)
 
 try:
     import json
-#use .json to retrieve data and stored as JSON object from the API and name it as data_retrieved.
+#use .json to retrieve data and stored as JSON object from the API and name it as data_retrieved
     data_retrieved=response.json()
     json.dumps(data_retrieved,indent=4)
 
-#extract Realtime Currency Exchange Rate from data_retrieved and set it to variable, Realtime_currency_exchange.
+#extract Realtime Currency Exchange Rate from data_retrieved and set it to variable, Realtime_currency_exchange
     Realtime_currency_exchange = data_retrieved["Realtime Currency Exchange Rate"]
-#extract data from exchange rate 
+#extract data from 5.Exchange rate from Realtime_currency_exchange and set it to variable, Exchange_Rate
     Exchange_Rate=(float(Realtime_currency_exchange["5. Exchange Rate"]))
-    print(Exchange_Rate)
+    #print(Exchange_Rate)
 
     import re
     from pathlib import Path
     # Import csv module
     import csv
 
+#assign file path to csv_reports and extend to overheads.csv
     file_path = Path.cwd()/"csv_reports"/"overheads.csv"
 
+#create an empty list known as full_list
     full_list=[]
+#`file_path` is a variable assigned to file after opening it
     with file_path.open(mode = "r",encoding = "UTF-8", newline="") as file:
+#use csv.reader to read data in the file
         reader = csv.reader(file)
+#ignore the first row of the data in overheads.csv
         next(reader)
         for line in reader:
             #print(line)
+#append line into full_list
             full_list.append(line)
     print(full_list)
 
+#create a variable known as highest value and assign 0 to it
     highest_value = 0
+#set full_lists to the variable known overheads
     overheads = full_list
+#create a for loop known as current_value
     for current_value in full_list:
-        if float(current_value[1]) > highest_value:
+#if current value is larger than or equal to the highest value, assign current_value to be the highest value
+        if float(current_value[1]) >= highest_value:
             highest_value = float(current_value[1])
+#extract the day in current_value and set it to a variable known as overheads
             overheads = current_value[0]
     def convertUSD_SGD(USD):        
         """
-        -This function will convert USD to SGD by multiplying exchange rate and return the converted value
+        -This function will convert USD to SGD by multiplying the exchange rate and return the converted value
         - one parameter required USD (as integer or float)
         """
         return USD * Exchange_Rate
+#create a variable known as SGD and assign the converted amount from USD to SGD into the variable, SGD
     SGD=(convertUSD_SGD(USD=highest_value))
-
-
-    print(overheads, SGD)
+    Overheads = (f'[HIGHEST OVERHEADS] {overheads}: SGD {SGD}')
+    print(Overheads)
 except Exception as e:
         print("An error has occured.\nReason:{e}")
--- profit_loss.py
+++ profit_loss.py
@@ -5,31 +5,51 @@ my_api = 'VSAFQ7CP9MDL4PCL'
 url= 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={my_api}' 
  
 response = requests.get(url) 
-print(response) 
+#print(response) 
 
 try: 
-    import json  
-    data=response.json()  
-    print(json.dumps(data,indent=4))  
-    
-    Real_Time_Currency_Rate =data["Realtime Currency Exchange Rate"]  
+    import json 
+#use .json to retrieve data and stored as JSON object from the API and name it as data_retrieved 
+    data_retrieved=response.json()  
+    print(json.dumps(data_retrieved,indent=4))  
+
+#extract Realtime Currency Exchange Rate from data_retrieved and set it to variable, Realtime_currency_exchange
+    Real_Time_Currency_Rate =data_retrieved["Realtime Currency Exchange Rate"]  
+#extract data from 5.Exchange rate from Realtime_currency_exchange and set it to variable, Exchange_Rate
     ExchangeRate=(float(Real_Time_Currency_Rate['5. Exchange Rate'])) 
-    
+    #print(Exchange_Rate)
+
+    import re
+    from pathlib import Path
+    # Import csv module
+    import csv
+
+#assign file path to csv_reports and extend to profit-and-loss-usd.csv
     file_path=Path.cwd()/"csv_reports"/"profit-and-loss-usd.csv" 
-    
+#`file_path` is a variable assigned to file after opening it 
     with file_path.open(mode = "r",encoding = "UTF-8", newline="") as file:  
-        reader = csv.reader(file)  
+#use csv.reader to read data in the file
+        reader = csv.reader(file) 
+#ignore the first row of the data in profit-and-loss-usd.csv 
         next(reader)  
+#create empty list known as profitloss
         profitloss=[]  
+#create empty list known as pnldata
         pnldata=[]  
         for value in reader:  
+#append value into pnldata
             pnldata.append(value)  
-        
-    prev_value=float(pnldata[0][4])  
+#create a variable known as prev_value and assign float value in pnldata   
+    prev_value=float(pnldata[0][4])
+#create a variable known as day and assign it to pnldata 
     day=pnldata 
+#crate a for loop known as current_value 
     for currentvalue in pnldata:  
+#if current value is larger than or equal to the prev_value, assign current_value to be the prev_value
         if float(currentvalue[4]) >= float(prev_value):  
-            prev_value=float(currentvalue[4])    
+            prev_value=float(currentvalue[4])  
+#if current value is smaller than prev_value, pre_value would minus the current_value to find the difference value
+#the difference value would be assign to a variable known as difference  
         else:  
             difference =  float(prev_value) - float(currentvalue[4])    
             def convertUSD_SGD(USD):          
@@ -38,13 +58,15 @@ try:
             - one parameter required USD (as integer or float)  
             """  
                 return USD * ExchangeRate  
+#create a variable known as SGD and assign the converted amount from USD to SGD into the variable, SGD
             SGD=(convertUSD_SGD(USD=difference))  
-    
-            day=currentvalue[0]  
-            prev_value=float(currentvalue[4])  
-            ProfitnLoss=(day,SGD)  
+#extract the day in current_value and set it to a variable known as day
+            day=currentvalue[0] 
+#extract the values in current_value and set it to a variable known as pre_value
+            prev_value=float(currentvalue[4]) 
+            ProfitnLoss = (f"[CASH DEFICIT] DAY: {day}, AMOUNT:SGD{SGD}")
+#append ProfitnLoss to profitloss
             profitloss.append(ProfitnLoss)  
-            prev_value=float(currentvalue[4])  
     
     print(profitloss)
 except Exception as e:
