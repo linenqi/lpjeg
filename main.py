ef3201c492c49b6cac122aa2a984ed6fde57f9b6
Gayathri0611 <srigayathri123@icloud.com>
2022-08-04 15:15:10 +0800

    Commit

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import profit_loss
 
 def main():
 
-        forex =api.api_function()
+        forex = api.api_function()
         overheads.overhead_function(forex)
         coh.coh_function(forex)
         profit_loss.profitloss_function(forex)
